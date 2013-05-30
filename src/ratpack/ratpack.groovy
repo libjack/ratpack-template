cbedff0aa94903db0911f49af56121ac8dd8c49d
John Liberty <libjackjl@gmail.com>
2013-05-30 02:47:40 -0400

    updated for 0.9.0-SNAPSHOT of Ratpack
    fix build.gradle, and default handlers, but need to figure out params

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,3 +1,10 @@
-get('/') {
-   render('index.html')
+import org.ratpackframework.groovy.templating.TemplateRenderer
+import static org.ratpackframework.groovy.RatpackScript.ratpack
+
+ratpack {
+ handlers {
+  get() {
+    get(TemplateRenderer).render "index.html", title: "mojo"
+  }
+ }
 }
