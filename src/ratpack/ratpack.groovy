da254f12ec1d97d37bb6fd10f6b1fe45e3118852
John Liberty <libjackjl@gmail.com>
2013-07-01 23:56:28 -0400

    cleanup and add better description for path

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -4,29 +4,40 @@ import static org.ratpackframework.groovy.RatpackScript.ratpack
 ratpack {
  handlers {
   get() {
-    get(TemplateRenderer).render "index.html", title: "mojo"
+    get(TemplateRenderer).render "index.html", title: "Template"
   }
 
   get("test") {
-    def params = request.getQueryParams()
-    def b = params.b
+    def params = request.queryParams
     def s = params.s ?: ''
+    // the simple form (or get(key)) will just get a single string
+    // to get all the values for l key, use getAll(key)
     def l = params.l ? params.getAll("l").join("|") : '|';
-    // if just get, the delagate will return a single string, 
-    //def l = params.get("l") ?: ''
-    response.send "test" + " params:" + params.toString() + " b:" + (b?'T':'F') + " s:" + s + " l:" + l
+    response.send "test" + " params:" + params.toString() + " -> s:" + s + " l:" + l
   }
 
   get("test/:a") {
-    def params = request.getQueryParams()
-    response.send "test" + " path:" + getAllPathTokens().toString() + " params:" + params.toString();
+    def params = request.queryParams
+    response.send "test" + " path:" + pathTokens.toString() + " params:" + params.toString();
   }
 
   get("test/:a/b/:b") {
-    def params = request.getQueryParams()
-    response.send "test/b" + " all path:" + getAllPathTokens().toString() + " path:" + getPathTokens().toString() + " params:" + params.toString();
+    def params = request.queryParams
+    response.send "test/b" + " all path:" + getAllPathTokens().toString() + " path:" + pathTokens.toString() + " params:" + params.toString();
   }
 
+  // show difference between allPathTokens and pathTokens
+  // do a get("test/a/b/b") get("test1/a/b/b")
+  prefix(":prefix") {
+    get(":a") {
+      response.send ":prefix/:a" + " allpath:" + allPathTokens.toString() + " path:" + pathTokens.toString()
+    }
+    get(":a/b/:b") {
+      response.send ":prefix/:a/b/:b" + " allpath:" + allPathTokens.toString() + " path:" + pathTokens.toString()
+    }
+  }
+
+  // favicon, static files
   assets "public"
  }
 }
